fc9302906582c280593d2754e581badc89c8e05e
Arvid Kristoffersson <arvid.kristoffersson@icloud.com>
2025-02-06 23:06:07 +0100

    Added 1 entire function #impact

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,19 @@
 use actix_web::{post, web, HttpResponse, HttpServer, Responder};
 use shared::FileUpload;
+use shared::EncryptedData;
 
 #[post("/upload")]
 async fn upload(_data: web::Json<FileUpload>) -> Result<impl Responder, actix_web::Error> {
     Ok(HttpResponse::Ok().body("File uploaded"))
+}
+
+let global_data: Vec<EncryptedData> = Vec::new();
+let global_counter: i32 = -1;
+
+fn add_data(encdata: EncryptedData) -> i32 {
+    global_data.append(encdata);
+    global_counter += 1;
+    return global_counter;
 }
 
 #[actix_web::main]
@@ -13,3 +23,4 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
+
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -5,3 +5,10 @@ pub struct FileUpload {
     pub filedata: Vec<u8>,
     pub signature: Vec<u8>,
 }
+
+#[derive(Debug)]
+struct EncryptedData {
+    pub data: Vec<u8>,
+    pub tag: [u8; 16],
+    pub nonce: NonceGen,
+}
