e51b4f5e63896dfe8103df23a7a5a2f9c9ecc4f5
Arvid Kristoffersson <arvid.kristoffersson@icloud.com>
2025-02-07 00:30:17 +0100

    Da goat strikes again #impact

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -3,6 +3,7 @@ use ring::{
     rand::{SecureRandom, SystemRandom},
 };
 use serde::{Deserialize, Serialize};
+use sha2::{Sha256, Digest};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct EncryptedData {
@@ -27,4 +28,47 @@ impl NonceSequence for NonceGen {
     fn advance(&mut self) -> Result<Nonce, ring::error::Unspecified> {
         Nonce::try_assume_unique_for_key(&self.0)
     }
+}
+
+pub struct MerkleTree {
+	pub MT: [String; 400001],
+	pub indexToNode: [i32; 100001]
+}
+
+impl MerkleTree {
+	pub fn initTree(&mut self, l: i32, r: i32, node: i32){ //initialize:a alltid med l=0, r=1e5, node=1
+		if l == r {
+			self.indexToNode[l as usize] = node;
+			return;
+		}
+		let mid = (l+r)>>1;
+		self.initTree(l, mid, 2*node);
+		self.initTree(mid+1, r, 2*node+1);
+	}
+	
+	fn hash_two_strings(s1: String, s2: String) -> String {
+	    let concatenated = format!("{}{}", s1, s2);
+	    let mut hasher = Sha256::new();
+	    hasher.update(concatenated);
+	    let result = hasher.finalize();
+	    
+	    format!("{:x}", result)
+	}
+	
+	pub fn update(&mut self, index: i32, hashed_data: String){
+		let mut node = self.indexToNode[index as usize];
+		self.MT[node as usize] = hashed_data;
+		node = node >> 1;
+		while node > 0 {
+			let a = 2*node;
+			let b = 2*node+1;
+			self.MT[node as usize] = Self::hash_two_strings(self.MT[a as usize].clone(), self.MT[b as usize].clone());
+			node = node >> 1;
+		}
+	}
+	
+	pub fn top_hash(&self) -> String {
+		return self.MT[1].clone();
+	}
+	
 }
